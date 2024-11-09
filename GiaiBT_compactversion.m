a8d8115b805ec9043ceedf0885ab896a3541ebe4
NguyenKhoa <nguyenbuinguyenkhoa@gmail.com>
2024-11-09 14:30:17 +0700

    update with apropriate solution

--- GiaiBT_compactversion.m
+++ GiaiBT_compactversion.m
@@ -14,8 +14,7 @@ B_c = [0;
 C_c = [1 0];
 D_c = 0;
 
-% Từ đó quy đổi về phương trình rời rạc của đối tượng với T_sample cho
-% trước
+% Từ đó quy đổi về phương trình rời rạc của đối tượng với T_sample
 sys_cont_represent = ss(A_c, B_c, C_c, D_c);
 sys_disc_represent = c2d(sys_cont_represent, T_sample, 'zoh');
 disp('Kết quả ma trận rời rạc của hệ');
@@ -24,25 +23,22 @@ B_d = sys_disc_represent.B
 C_d = sys_disc_represent.C
 D_d = sys_disc_represent.D
 
-disp('Kích thước biến trạng thái: ')
-states_size = size(A_d, 1)
-
-disp('Kích thước ngõ vào: ')
-inputs_size = size(B_d, 2)
-
-disp('Kích thước ngõ ra: ')
+states_size = size(A_d, 1);
+inputs_size = size(B_d, 2);
 outputs_size = size(C_d, 1)
-
-Q = eye(states_size);
-R = eye(outputs_size);
-Q(1,1) = 2;
 
 % Khởi tạo theta
 A = A_d
 B = B_d
 C = C_d
 D = D_d
-x=[10;2.2];
+
+x=[0;0.1];
+
+Q = eye(states_size);
+Q(1,1) = 2;
+
+R = eye(outputs_size);
 theta_size = 6;
 theta = rand(theta_size,1); % Theta là vector cột
 H22 = theta(theta_size);
@@ -50,9 +46,10 @@ H21 = [theta(3); theta(5)];
 K=inv(H22)*H21';
 t=0;
 gama=1;
-N=10;
+N=30;
+K_data_holder = zeros(2,1,N);
 xx=K';
-for k=1:N
+for k=1:N   
     P = eye(theta_size,theta_size);
     Z=[];
     Y=[];
@@ -70,8 +67,27 @@ for k=1:N
     end
     theta=(Z'*Z)\Z'*Y;
     H22=theta(theta_size);
-    H_21 = [theta(3); theta(5)];
+    H21 = [theta(3)/2; theta(5)/2];
     K=H22\(H21');
+    K_data_holder(:,:,k) = K;
 end
 K
 [S,K_LQR]=dlqr(A,B,Q,R)
+
+% Extract the data for K1 and K2
+K1 = squeeze(K_data_holder(1, 1, :)); % Row 1 data across slices
+K2 = squeeze(K_data_holder(2, 1, :)); % Row 2 data across slices
+% Define the x-axis (entry indices)
+x = 1:N;
+% Plot K1 and K2 against the entry index
+figure;
+plot(x, K1, '-o');hold on;
+plot(x, K2, '-s');
+hold off;
+xlabel('Lần lặp');
+ylabel('Giá trị');
+title('Đồ thị hội tụ K_1 và K_2', 'Interpreter', 'tex');
+legend({'K_1', 'K_2'}, 'Interpreter', 'tex');
+grid on;
+legend show;
+print('K1_K2_plot_entries', '-dsvg');
--- RLLQRroirac.m
+++ RLLQRroirac.m
@@ -1,71 +1,74 @@
 clear
 clc
 
-B=[ 0.0450;    0.1038;    0.7378;    0.0007];
+B=[ 0.0000;
+    0.0100];
 
-A=[0        1.0047      0.0867      -0.0450;  
-   -0.0739  0.7490      0.1154      -0.1038;
-   -0.5354  -0.3401     0.2303      -0.7378;
-   0.0593   0.0316      0.0020      0.9993];
+A = [1.0000    0.0100;
+   -0.0005    0.9990];
 
-Q=[10 0 0 0;
-    0 1 0 0;
-    0 0 10 0;
-    0 0 0 1];
+
+Q=[1 0;
+    0 1];
 R=1;
-x=[0;0.1;0;0];
-T_sample = 1;
+
+x=[0;0.1];
+T_sample = 0.01;
 
 xx=[];
 uu=[];
 
 
-%K=[1 1 1 1];
-%K=[rand rand rand rand];
-
-% Equal to zeros because we don't need initialize parameters's controller
-K=[0 0 0 0];
-
-theta=[rand rand rand rand 2*K(1) rand rand rand 2*K(2) rand rand 2*K(3) rand  2*K(4) 1 ]';
-% theta=[rand rand rand rand rand rand rand rand rand rand rand rand rand rand rand ]';
-H22=theta(15);
-H21=[theta(5)/2; theta(9)/2; theta(12)/2;theta(14)/2];
+x_tichluy = [];
+theta = rand(6,1);
+H22=theta(6);
+H21 = [theta(3)/2; theta(5)/2];
 K=inv(H22).*H21';
 t=0;
 gama=1;
-N=4;
+N=15;
+K_data_holder = zeros(2,1,N);
 xx=K';
 for k=1:N
-P=eye(15,15);
-    Z=[];
+P=eye(6,6);
+Z=[];
     Y=[];
     for i=1:30
          explore=rand;
          xold=x;
          sim('hethong_RR1.mdl');
-         %Phi_1 này là phi lấy bước trước đó
-         % và được chọn
-         phi_1=[xold(1)^2;xold(1)*xold(2);xold(1)*xold(3);xold(1)*xold(4);xold(1)*u;xold(2)^2;xold(2)*xold(3);xold(2)*xold(4);xold(2)*u;xold(3)^2; xold(3)*xold(4);xold(3)*u;xold(4)^2;xold(4)*u;u^2];
-         r=[xold(1);xold(2);xold(3);xold(4)]'*Q*[xold(1);xold(2);xold(3);xold(4)]+u*R*u;
+         x_tichluy = [x_tichluy, x];
+         phi_1 = [xold(1)^2; xold(1)*xold(2); xold(1)*u; xold(2)^2; xold(2)*u; u^2];
+         r=[xold(1);xold(2)]'*Q*[xold(1);xold(2)]+u*R*u;
          u=-K*x;
-         phi_2=[x(1)^2;x(1)*x(2);x(1)*x(3);x(1)*x(4);x(1)*u;x(2)^2;x(2)*x(3);x(2)*x(4);x(2)*u;x(3)^2;x(3)*x(4); x(3)*u;x(4)^2;x(4)*u;u^2];
+         phi_2=[x(1)^2;x(1)*x(2);x(1)*u;x(2)^2;x(2)*u;u^2];
          phi=phi_1-phi_2;
-     %    m=(phi'*P'*phi);
-      %   theta=theta+(P*phi*(r-phi'*theta))/(m);
-     %    P=P-(P*phi*phi'*P)/(m);
-    %     t=t+1;
           Z=[Z; phi']; 
           Y=[Y; r];
     end
     theta=(Z'*Z)\Z'*Y;
-    H22=theta(15);
-    H21=[theta(5)/2; theta(9)/2; theta(12)/2;theta(14)/2];
-   % S=1/theta(10).*[theta(1) theta(2)/2 theta(3)/2;theta(2)/2 theta(5) theta(6)/2; theta(3)/2 theta(6)/2 theta(8)];
- %   S=[theta(1) theta(2)/2 theta(3)/2;theta(2)/2 theta(5) theta(6)/2; theta(3)/2 theta(6)/2 theta(8)]
+    H22=theta(6);
+    H21 = [theta(3)/2; theta(5)/2];
     K=H22\(H21')
+    K_data_holder(:,:,k) = K;
 
 
 end
 
 [S,K_LQR]=dlqr(A,B,Q,R)
 %plot(1:N+1,xx(1,:),1:N+1,xx(2,:))
+
+K1 = squeeze(K_data_holder(1, 1, :)); % Row 1 data across slices
+K2 = squeeze(K_data_holder(2, 1, :)); % Row 2 data across slices
+x_plot = 1:N;
+figure;
+plot(x_plot, K1, '-o');hold on;
+plot(x_plot, K2, '-s');
+hold off;
+xlabel('Lần lặp');
+ylabel('Giá trị');
+title('Đồ thị hội tụ K_1 và K_2', 'Interpreter', 'tex');
+legend({'K_1', 'K_2'}, 'Interpreter', 'tex');
+grid on;
+legend show;
+print('K1_K2_plot_entries', '-dsvg');
